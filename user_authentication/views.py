c8223541c3323f27849fa5ddc7557219d296decb
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-01-19 11:13:49 +0530

    Create Login Page

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django import forms
+from django.contrib import messages
 
 class CustomUserCreationForm(UserCreationForm):
   username = forms.CharField(
@@ -18,15 +19,25 @@ def register_view(request):
   if request.method == 'POST':
     form = CustomUserCreationForm(request.POST)
     if form.is_valid():
-      user = form.save()
-      login(request,user)
-      return 'Success'
+      form.save()
+      return redirect('login')
   else:
     form = CustomUserCreationForm()
   return render(request, 'user_authentication/register.html', {'form': form})
 
 def login_view(request):
-  pass
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'Invalid username or password.')
+  return render(request, 'user_authentication/login.html')
 
 def logout_view(request):
-  pass
+  logout(request)
+  return redirect('login')
