25c546d7af895937104127dc1a8bf3e7b5eac08a
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-01-24 17:51:54 +0530

    Create View For Cricket Homepage

--- cricket/urls.py
+++ cricket/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('')
+  path('',view=views.cricket_home_page,name='cricket_home')
 ]
--- cricket/views.py
+++ cricket/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .api_functions import CricketDataAPI
 
 # Create your views here.
+def cricket_home_page(request):
+  try:
+    cricket_api = CricketDataAPI()
+    current_matches = cricket_api.get_current_matches()
+    cricket_series = cricket_api.get_series()
+    
+    context = {
+      'current_matches': current_matches,
+      'cricket_series': cricket_series
+    }
+    return render(request, 'cricket_home.html', context)
+    
+  except Exception as e:
+    context = {
+      'error_message': str(e),
+      'current_matches': [],
+      'cricket_series': []
+    }
+    return render(request, 'cricket_home.html', context)
