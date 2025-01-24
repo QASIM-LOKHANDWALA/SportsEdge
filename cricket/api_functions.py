d941bf82cb18fc221758436dd9fe0f5870282ae7
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-01-24 17:46:09 +0530

    Create Method To Get Series List From API

--- cricket/api_functions.py
+++ cricket/api_functions.py
@@ -9,7 +9,6 @@ class CricketDataAPI:
   
   def __init__(self):
     self.api_key = os.getenv('CRICKET_DATA_API_KEY')
-    print(self.get_current_matches())
   
   def get_current_matches(self):
     url = f"{self.BASE_URL}currentMatches"
@@ -32,4 +31,31 @@ class CricketDataAPI:
     except requests.RequestException as e:
       print(f"Request error: {e}")
       return []
+    except Exception as e:
+      print(f"An error occured : {e}")
+      return []
     
+  def get_series(self):
+    url = f"{self.BASE_URL}series"
+    
+    try:
+      response = requests.get(
+        url, 
+        params={'apikey': self.api_key}
+      )
+      
+      if response.status_code == 200:
+        data = response.json()
+        series = data.get('data')
+        print(series)
+        return series
+      
+      else:
+        print(f"Error fetching matches: {response.status_code}")
+        return []
+    except requests.RequestException as e:
+      print(f"Request error: {e}")
+      return []
+    except Exception as e:
+      print(f"An error occured : {e}")
+      return []
